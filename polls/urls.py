6b820944b71a205bb7058b13de5db7d086abfdcc
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2021-04-03 20:49:53 +0300

    part3 views

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'  # Allows django to differentiate urls names between apps
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question>/results/', views.result, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+
 # Path function has different arguments
 # Path route - string containing url pattern
 # Path view - binds certain view to the url
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("We are at the index poll")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list }  # The context is a dictionary mapping template variable names to Python objects.
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id) # shortcut to avoid try-catch
+    return render(request, 'polls/details.html', { 'question': question })
+
+def result(request, question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
+
