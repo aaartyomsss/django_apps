6ed82a76a2578f1817f2341c58dc2caa39745533
aaartyomsss <aaartyomsss@gmail.com>
2021-05-03 21:45:10 +0300

    part 5

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published') # provide "human-readable name for the column"
@@ -10,7 +11,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # ea choice corresponds to question
